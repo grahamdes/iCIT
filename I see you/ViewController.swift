b480d3d7eb0c517ae468571a835e9972ffead008
grahamdes <grahamdesmond13@gmail.com>
2016-06-16 17:29:05 -0700

    Initial Commit

---
+++ I see you/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I see you
+//
+//  Created by Graham Desmond on 6/16/16.
+//  Copyright © 2016 GrahamApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
